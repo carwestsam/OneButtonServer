16bb6b9bc20537596a6e99d83e6e2cdb12eefa33
carwestsam <carwestsam@gmail.com>
2015-02-11 23:41:39 +0800

    ss

--- myblog/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask import request
-
-app = Flask( __name__ )
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:cASDqwe@localhost/only'
-app.config['SQLALCHEMY_ECHO'] = True
-
-import  myblog.views
